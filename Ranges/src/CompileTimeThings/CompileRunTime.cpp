91730e6919751ea744cb47b486a6f17e4404ca1b
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-11-21 00:34:45 +0300

    started more Compile time examples; just a basic string length function(similar to strlen)

--- Ranges/src/CompileTimeThings/CompileRunTime.cpp
+++ Ranges/src/CompileTimeThings/CompileRunTime.cpp
@@ -4,5 +4,8 @@
 void CompileTimevsRunTime()
 {
 	std::puts("--CompileTimevsRunTime--");
+	constexpr char name[]{ "Didoski" };
+	constexpr auto length = StrLength(name);
+	std::printf("String Length without null character: %zu \n", length);
 
 }
--- Ranges/src/CompileTimeThings/CompileRunTime.hpp
+++ Ranges/src/CompileTimeThings/CompileRunTime.hpp
@@ -1,3 +1,11 @@
 #pragma once
 
 void CompileTimevsRunTime();
+
+
+consteval std::size_t StrLength(const char* str)
+{
+
+	return *str ? 1 + StrLength(str + 1) : 0;
+	
+}
