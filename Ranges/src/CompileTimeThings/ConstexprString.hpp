f46eac1a46021233aaf281b40b59a9f69dcf3c4f
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-12-02 15:27:33 +0300

    added more function for SustomFormatter to do type check both compile and runtime to be used safely in functions like printf; in progress

--- Ranges/src/CompileTimeThings/ConstexprString.hpp
+++ Ranges/src/CompileTimeThings/ConstexprString.hpp
@@ -1,10 +1,18 @@
 #include "RangesHeaders.hpp"
 
-class string
-{
-
-
-
-
-
-};
+#if defined (__GNUG__)   || defined(__clang__)
+	# if not (__cpp_lib_constexpr_string > 201911)
+	class string
+	{
+	
+	
+	
+	
+	
+	};
+	#else
+		using std::string;
+	#endif
+#else
+	using std::string;
+#endif
--- Ranges/src/CompileTimeThings/CustomFormatterTypeSafe.hpp
+++ Ranges/src/CompileTimeThings/CustomFormatterTypeSafe.hpp
@@ -1,5 +1,34 @@
 #include "RangesHeaders.hpp"
 #include "ConstexprString.hpp"
 
+template<typename T, class... Args>
+constexpr void Formatter(string& buffer, std::string_view v, const T& t, const Args&... args)
+{
+
+}
+
+
+template<typename... Args>
+struct basic_format_string
+{
+	std::string_view str;  
+
+	// Alternative is to use std::is_constructible_v to allow explicit convertible types
+	// std::is_convertible_t is only for implicitly covertible types
+	template<class T>
+	requires std::is_convertible_v<T&, std::string_view>
+	consteval basic_format_string(const T& s): str{s} 
+	{
+		string out{};
+		Formatter(out, str, Args{}...);
+	}
+};
+
+
+
+template<typename... Args>
+using format_string = basic_format_string<std::type_identity_t<Args>...>;
+
+
 template<class... Args>
-string  format(std::string_view fmt, const Args&... args);
+string  format(format_string<Args...> fmt, const Args&... args);
