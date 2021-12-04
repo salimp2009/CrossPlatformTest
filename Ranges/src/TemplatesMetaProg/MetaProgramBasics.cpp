d7f0c3579590e8402677044b2aeb4a0a53c06edb
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-12-04 21:30:13 +0300

    added more examples for basic type_traits and meta programming techniques ; mainly using std::void_t to determine a type is valid; void_t<T> return if type T is valid

--- Ranges/src/TemplatesMetaProg/MetaProgramBasics.cpp
+++ Ranges/src/TemplatesMetaProg/MetaProgramBasics.cpp
@@ -17,6 +17,18 @@ void MetaProgramming_Test()
 	// gives a compiler hard error but it should not; it should only fail the static assert
 	//static_assert(std::is_same_v<void, std::void_t<void&>>);
 
+	typeA<int> myintA;
+	static_assert(std::is_same_v<A<int>, decltype(myintA)>);
+	auto result = std::is_same_v<A<int>, decltype(myintA)>;
+	std::printf("%s \n", (result ? "true":"false"));
 
+	typeA<int, float, double> mymultitypeA{};
+	result = std::is_same_v<A<int, float, double>, decltype(mymultitypeA)>;
+	std::printf("%s \n", (result ? "true" : "false"));
+
+	static_assert(std::is_same_v<int&, typename Add_Lvalue_reference<int>::type>);
+	static_assert(std::is_same_v<void, typename Add_Lvalue_reference<void>::type>);
+	
+	static_assert(std::is_same_v<void, typename std::add_lvalue_reference_t<void>>);
 
 }
--- Ranges/src/TemplatesMetaProg/MetaProgramBasics.hpp
+++ Ranges/src/TemplatesMetaProg/MetaProgramBasics.hpp
@@ -22,9 +22,22 @@ struct HasTypeMember<T, std::void_t<typename T::type>> : std::true_type{};
 
 // experimental; trying to use type_identiy to add member to struct A called type which is the type of
 // Args passed in struct A; e.g : A<int>
-template<typename Args>
+template<typename... Args>
 struct A {};
 
 template<typename... Args>
 using typeA =A<TypeIdentity_t<Args>...>;
+
+template<typename T, typename=void>
+struct Add_Lvalue_reference: TypeIdentity<T>{ };
+
+template<typename T>
+struct Add_Lvalue_reference<T, std::void_t<T&>>: TypeIdentity<T&>{ };
+
+template<typename T>
+using Add_Lvalue_reference_t = typename Add_Lvalue_reference<T>::type;
+
+
+
+
 
