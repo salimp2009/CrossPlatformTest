3511bb5bdd0ee5f713ea6dbb14d6e60220c8a531
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-12-08 23:58:49 +0300

    started NamedTupple example by fixed_string which is const char[] and used operator std::string_view() to be able to convert to string_view; added a deduction guide (not needed since C++20)

--- Ranges/src/NTTP/NamedTupple.cpp
+++ Ranges/src/NTTP/NamedTupple.cpp
@@ -5,5 +5,14 @@ void NamedTupple_Test()
 {
 	std::puts("--Named Tupple--");
 
+	// TDD; Test Driven Development ;write the test before implementation
+	//static_assert(""sv == fixed_string(""));
+	//static_assert("name"sv == fixed_string("name"));
 
+	auto fixedStrng1 = fixed_string("name");
+
+	std::string_view val = fixedStrng1;
+
+	std::printf("%s %lu \n" , fixedStrng1.data, fixedStrng1.size);
+	std::printf("%s %lu \n" , val.data(), val.size());
 }
--- Ranges/src/NTTP/NamedTupple.hpp
+++ Ranges/src/NTTP/NamedTupple.hpp
@@ -1,3 +1,40 @@
 #include "RangesHeaders.hpp"
 
 void NamedTupple_Test();
+
+template<typename TChar, std::size_t N>
+struct fixed_string
+{
+	TChar data[N]{};
+	static constexpr std::size_t size = N;
+
+	constexpr explicit(false) fixed_string(const TChar(&str)[N])
+	{
+		std::copy_n(str, N, data);
+	}
+
+	// Used in the original example ; the above method is nicer to me :)
+	// if you use both then compiler error for ambigiues overloading
+	//constexpr explicit(false) fixed_string(const TChar* str)
+	//{
+	//	std::copy_n(str, N+1, data);
+	//}
+
+	constexpr explicit(false) operator std:: string_view() const
+	{
+		return { data, N-1 };
+	}
+
+	
+	//constexpr explicit(false) operator const auto*() const
+	//{
+	//	return data;
+	//}
+};
+
+template<std::size_t N>
+fixed_string(const char(&)[N])->fixed_string<char, N>;
+
+
+
+
