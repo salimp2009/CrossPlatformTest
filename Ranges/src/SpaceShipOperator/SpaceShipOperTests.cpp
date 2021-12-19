2febcbe5126502af4631e38cc3ecfa96b99cc7dd
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-12-19 16:39:50 +0300

    added some basic examples for ordering type; strong_ordering; weak_ordering; and partial_ordering

--- Ranges/src/SpaceShipOperator/SpaceShipOperTests.cpp
+++ Ranges/src/SpaceShipOperator/SpaceShipOperTests.cpp
@@ -10,6 +10,29 @@ void SpaceShipOper_Test1()
 	static_assert(not (compA == compB));
 	static_assert(compA <= compB);
 	static_assert(not (compA > compB));
+}
+
+void ComparisionTypes_Ordering()
+{
+	std::puts("--ComparisionTypes_Ordering--");
+
+	constexpr int x = 45;
+	constexpr int y = 17;
+
+	static_assert(((x <=> y)== std::strong_ordering::greater));
+	static_assert(((x <=> 45.0)== std::partial_ordering::equivalent));
+	static_assert(((&x<=>&x)== std::strong_ordering::equal));
+	
+	// Comparision with <=> operator is not supported !!
+	//static_assert(((&x<=>nullptr)== std::strong_ordering::equal));
 
 
+	static_assert(not ((x <=> y) == std::partial_ordering::equivalent));
+
+	
+	// this is always OK; 
+	if (x <=> y == std::partial_ordering::equivalent) 
+	{ };
+
+	
 }
