3c054d8d7236a7f88b96e0ffa098a68e180afe83
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-01-01 15:24:02 +0300

    added a basic example of function with concept auto however if the function definition is in the header file it did not compile but it compiles if it was in the cpp file ??? also made a similar example using a concept in the standar std::integral ; it compiles if it is in the header file or not

--- Concepts/src/ConceptsRequires/ConceptRequires.hpp
+++ Concepts/src/ConceptsRequires/ConceptRequires.hpp
@@ -21,9 +21,31 @@ constexpr T minnVal(T a, T b)
 	return b < a ? b : a;
 }
 
+// constraint function
 template<typename T>
 requires IsPointer<T>
 constexpr T minnVal(T a, T b)
 {
 	return *b < *a ? b : a;
+}
+
+// type constraint
+template<IsPointer T>
+constexpr auto minnVal2(T a, T b)
+{
+	return *b < *a ? *b : *a;
+}template<IsPointer T>
+
+// this does not compile if in header file but compiles in cpp
+inline constexpr auto minnVal3(IsPointer auto a, IsPointer auto b)
+{
+	return *b < *a ? *b : *a;
+	// Alternative
+	return minnVal(*a, *b);
+}
+
+//this compiles ???
+constexpr auto minnVal4(std::integral auto a, std::integral auto b)
+{
+	return minnVal(a, b);
 }
--- Concepts/src/ConceptsRequires/ConceptsRequires.cpp
+++ Concepts/src/ConceptsRequires/ConceptsRequires.cpp
@@ -2,6 +2,7 @@
 #include "ConceptRequires.hpp"
 
 
+
 void ConceptsRequirest_Test()
 {
 	std::puts("--ConceptsRequirest_Test--");
@@ -15,7 +16,18 @@ void ConceptsRequirest_Test()
 	//maxxValue(&val1, &val2);
 
 	constexpr auto result2 = minnVal(val1, val2);
-	std::printf("result2 minnVal: %i\n", result2);
+	std::printf("minnVal: %i\n", result2);
 	minnVal(&val1, &val2);
+
+	const int* p1 = &val1;
+	const int* p2 = &val2;
+	std::printf("minVal2: %i \n", minnVal2(p1, p2));
+
+	// gives a compiler error if the minnVal3() is defined in a seperate header because cannot deduce the types ??
+	// if minnVal3() is defined in the same cpp file it is OK
+	//auto result3 = minnVal3(p1, p2);
+	//std::printf("minVal3: %i", result3);
+
+	minnVal4(5, 45);
 
 }
